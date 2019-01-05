2ee1e8e981b83e2b04bf91f362cd248b1e43af9c
Kévin Gossé <gossekevin1@gmail.com>
2019-01-05 16:04:44 +0100

    add test content for app-main section

--- src/router.js
+++ src/router.js
@@ -11,16 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Layout
+      component: Layout,
+      children: [{
+        path: '',
+        component: () => import('@/views/About')
+      }]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
   ]
 })
