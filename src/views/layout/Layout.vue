c0f7a9ee2cf662d63ad99b087676eee73ec99d90
Kévin Gossé <gossekevin1@gmail.com>
2019-01-05 13:21:45 +0100

    navbar moved to src/components

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -6,7 +6,8 @@
 </template>
 
 <script>
-import { Navbar, AppMain } from './components'
+import { AppMain } from './components'
+import Navbar from '@/components/Navbar'
 
 export default {
   name: 'Layout',
